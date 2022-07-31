106678520002e03978750b48d76b998f8b147ad0
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-07-31 20:51:10 +0300

    fix: response

--- src/components/ImageGalleryApi/ImageGalleryApi.js
+++ src/components/ImageGalleryApi/ImageGalleryApi.js
@@ -9,6 +9,37 @@ const ImageGallery = ({ query, page, updateData, showButton }) => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const getResponse = async () => {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=26970425-ccd1377388b76d413dfca163b&q=${query}&image_type=foto&orientation=horizontal&safesearch=true&per_page=12&page=${page}`
+      );
+      console.log(response.data.hits);
+
+      if (response.data.total === 0) {
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again. `
+        );
+        showButton(false);
+        return;
+      }
+
+      if (page * 12 > response.data.total) {
+        showButton(false);
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      setImages(state => [...state, ...response.data.hits]);
+    } catch (err) {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again. `
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (query === '') {
       return;
@@ -18,39 +49,18 @@ const ImageGallery = ({ query, page, updateData, showButton }) => {
     }
     setLoading(true);
 
-    axios
-      .get(
-        `https://pixabay.com/api/?key=26970425-ccd1377388b76d413dfca163b&q=${query}&image_type=foto&orientation=horizontal&safesearch=true&per_page=12&page=${page}`
-      )
-      .then(response => {
-        showButton(true);
-        if (response.data.total === 0) {
-          Notiflix.Notify.failure(
-            `Sorry, there are no images matching your search query. Please try again. `
-          );
-          showButton(false);
-          return;
-        }
+    getResponse();
 
-        if (page * 12 > response.data.total) {
-          showButton(false);
-          Notiflix.Notify.failure(
-            "We're sorry, but you've reached the end of search results."
-          );
-        }
-
-        setImages(state => [...state, ...response.data.hits]);
-      })
-      .catch(function () {
-        // showButton(false);
-        Notiflix.Notify.failure(
-          `Sorry, there are no images matching your search query. Please try again. `
-        );
-      })
-      .then(function () {
-        setLoading(false);
-      });
-  }, [query, page, showButton]);
+    // .catch(function () {
+    //   // showButton(false);
+    //   Notiflix.Notify.failure(
+    //     `Sorry, there are no images matching your search query. Please try again. `
+    //   );
+    // })
+    // .then(function () {
+    //   setLoading(false);
+    // });
+  }, [query, page]);
 
   return (
     <>
